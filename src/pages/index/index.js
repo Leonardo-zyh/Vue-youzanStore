fd8ffb81e7ca44706bce0795d6dce0f1a00db110
Leonardo_zyh <956731563@qq.com>
2018-10-31 19:21:21 +0800

    loading

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,10 +3,13 @@ import './index.css'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+// import App from './App'
 import router from './router'
 import axios from 'axios'
 import url from '@/modules/js/api.js'
+import { InfiniteScroll } from 'mint-ui';
+
+Vue.use(InfiniteScroll);
 
 Vue.config.productionTip = false
 
@@ -15,16 +18,37 @@ Vue.config.productionTip = false
   new Vue({
   el: '#app',
   data:{
-    lists:null
+    lists:null,
+    pageNum:1,
+    loading:false,
+    allLoaded:false,
+    pageSize:6
   },
   created() {
+    this.getLists()
+  },
+  methods:{
+    getLists(){
+      this.loading = true
     axios.post(url.hotLists,{
-      pageNum:1,
-      pageSize:6,
+      pageNum:this.pageNum,
+      pageSize:this.pageSize,
     }).then(res=>{
       console.log(res);
-      this.list = res.data.lists
+
+      let curLists = res.data.lists 
+      if(curLists.length < this.pageSize ){
+        this.allLoaded = true
+      }
+      if(this.lists){
+        this.lists = this.lists.concat(curLists)
+      }else{
+        this.lists = curLists
+      }
+      this.loading = false
+      this.pageNum +=1
     })
+    }
   },
   router,
   // components: { App },
