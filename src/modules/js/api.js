fe2a08f25144fcf4e3accec7396972fd28132600
Leonardo_zyh <956731563@qq.com>
2018-11-24 18:28:37 +0800

    打包上传

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -20,7 +20,8 @@ let url =  {
 }
 //开发环境和真是环境
 
-let host = 'http://rap2api.taobao.org/app/mock/7058'
+let host = '//rap2api.taobao.org/app/mock/7058'
+
 for(let key in url){
     if(url.hasOwnProperty(key)){
        url[key] = host + url[key]
